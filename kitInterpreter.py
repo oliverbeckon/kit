8a5377cc5a9d5f5d620085ce3c9011a552de5f05
oliverbeckon <oliverbeckon@gmail.com>
2024-12-02 13:13:22 -0700

    renamed files

--- kitInterpreter.py
+++ kitInterpreter.py
@@ -1,4 +1,4 @@
-from parser import *
+from kitParser import *
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import lexer
-import interpreter
-import parser
+import kitLexer
+import kitInterpreter
+import kitParser
 # Create a lexer with the source code
 source_code = """
 
@@ -22,14 +22,14 @@ test2(100000)
 """
 
 # Step 1: Tokenize the source code
-Mylexer = lexer.Lexer(source_code)
+Mylexer = kitLexer.Lexer(source_code)
 tokens = Mylexer.get_tokens()
-#print(tokens)
+print(tokens)
 
 # Step 2: Parse the tokens into an AST
-Myparser = parser.Parser(tokens)
+Myparser = kitParser.Parser(tokens)
 ast = Myparser.parse()
 
 # Step 3: Interpret the AST
-Myinterpreter = interpreter.Interpreter()
+Myinterpreter = kitInterpreter.Interpreter()
 Myinterpreter.interpret(ast)
